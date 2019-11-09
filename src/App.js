f29a86e02b8ac5cd126f943a410b0c1c39b879cf
Douglas Ross <sachioross@gmail.com>
2019-11-08 21:34:37 -0800

    Updating dependecies, adding Bulma compoennts, connecting actxion

--- src/App.js
+++ src/App.js
@@ -5,11 +5,21 @@ import Board from './components/Board';
 import Analysis from './components/Analysis';
 import 'react-bulma-components/dist/react-bulma-components.min.css';
 import './css/board.css';
-import {Container} from 'react-bulma-components';
+import {Container, Columns, Button} from 'react-bulma-components';
 
 function App() {
+
+  function refresh (e) {
+    console.log(e);
+  }
+
   return (
     <Container>
+      <Columns>
+        <Columns.Column>
+          <Button onClick={(e) => refresh(e)}>Refresh</Button> 
+        </Columns.Column>
+      </Columns>
       <Board></Board>
       <Analysis></Analysis>
     </Container>
